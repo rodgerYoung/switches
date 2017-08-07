e8ec33a80e0efbe51782e14e75c8ec015846d1ce
yangbiao <929730168@qq.com>
2017-08-07 16:20:20 +0800

    滑动效果

--- sample/src/main/java/com/custom/activity/MainActivity.java
+++ sample/src/main/java/com/custom/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.custom.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.custom.switches.SwitchView;
 
@@ -13,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         switchView=(SwitchView)findViewById(R.id.my_switch);
+        switchView.setStatus(new String[]{"备用","备用","正常"});
+        switchView.setOnClickStateChange(new SwitchView.OnClickStateChange() {
+            @Override
+            public void onStateChange(int state, String statusText) {
+                Toast.makeText(getApplicationContext(),statusText,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- switch/src/main/java/com/custom/switches/SwitchView.java
+++ switch/src/main/java/com/custom/switches/SwitchView.java
@@ -5,13 +5,13 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 /**
  * 多个开关自定义view
@@ -19,16 +19,16 @@ import android.widget.Toast;
  */
 
 public class SwitchView extends View {
-    private int textColor = 0;
-    private int btnColor = 0;
-    private int borderColor = 0;
+    private int textColor = 0;//文字颜色
+    private int btnColor = 0;//按钮颜色
+    private int borderColor = 0;//背景
     private Context context;
     private float x = 0;
     private float height = 0;
-    private String[] status = {"你好", "你也好", "呵呵"};
+    private String[] status = {"开", "关"};
     private int state = 0;
     private String text = status[state];
-
+    private OnClickStateChange onClickStateChange;
     public SwitchView(Context context) {
         super(context);
     }
@@ -37,14 +37,12 @@ public class SwitchView extends View {
         super(context, attrs);
         this.context = context;
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, com.custom.switches.R.styleable.SwitchView, 0, 0);
-        int btnColor = typedArray.getColor(R.styleable.SwitchView_btn_color, Color.parseColor("#999999"));
-        int borderColor = typedArray.getColor(R.styleable.SwitchView_border_color, Color.parseColor("#cccccc"));
-        int textColor = typedArray.getColor(R.styleable.SwitchView_text_color, Color.parseColor("#cccccc"));
-
+        int btnColor = typedArray.getColor(R.styleable.SwitchView_btn_color, Color.WHITE);
+        int borderColor = typedArray.getColor(R.styleable.SwitchView_border_color, Color.parseColor("#4bd763"));
+        int textColor = typedArray.getColor(R.styleable.SwitchView_text_color, Color.parseColor("#000000"));
         this.btnColor = btnColor;
         this.borderColor = borderColor;
         this.textColor = textColor;
-
         typedArray.recycle();
     }
 
@@ -62,7 +60,7 @@ public class SwitchView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         //默认的宽度和高度
-        int height = measureDimension(50, heightMeasureSpec);
+        int height = measureDimension(100, heightMeasureSpec);
         this.height = height;
         x = height / 2;
         int width = height * status.length;
@@ -91,17 +89,28 @@ public class SwitchView extends View {
         return result;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        setBackgroundColor(Color.WHITE);
-        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        GradientDrawable gd = new GradientDrawable();//创建drawable
+        gd.setColor(borderColor);
+        gd.setCornerRadius(height);
+        setBackground(gd);
+
+        paint.setColor(borderColor);
         canvas.drawCircle(x, this.getHeight() / 2, height / 2, paint);
-        paint.setColor(Color.WHITE);
-        paint.setTextSize(height / 8);
+
+        paint.setColor(btnColor);
+        canvas.drawCircle(x, this.getHeight() / 2, height / 2-3, paint);
+
+        paint.setColor(textColor);
+        paint.setTextSize(height / 4);
+        paint.setStrokeWidth(4);
         for (int i = 0; i < status.length; i++) {
-            canvas.drawText(status[i], height * (i + 1) - height / 2 - text.length() * (height / 16), height / 2 + (height / 16), paint);
+            canvas.drawText(status[i], height * (i + 1) - height / 2 - status[i].length() * (height / 8), height / 2 + (height /8), paint);
         }
     }
 
@@ -111,7 +120,13 @@ public class SwitchView extends View {
             new Thread() {
                 @Override
                 public void run() {
-                    int onState = (int) (event.getX() / height);
+                    post(new Runnable() {
+                        @Override
+                        public void run() {
+//                            Toast.makeText(context,getLeft()+"",Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    int onState = (int) ((event.getX()-getLeft())/ height);
                     if (state != onState) {
                         if (state > onState) {
                             for (int i = 0; i < (state - onState) * height; i++) {
@@ -142,7 +157,9 @@ public class SwitchView extends View {
                     post(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getContext(), state + "", Toast.LENGTH_LONG).show();
+                            if (onClickStateChange!=null){
+                                onClickStateChange.onStateChange(state,status[state]);
+                            }
                         }
                     });
                 }
@@ -151,4 +168,43 @@ public class SwitchView extends View {
         return super.onTouchEvent(event);
     }
 
+    public String[] getStatus() {
+        return status;
+    }
+
+    public void setStatus(String[] status) {
+        this.status = status;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public int getBtnColor() {
+        return btnColor;
+    }
+
+    public void setBtnColor(int btnColor) {
+        this.btnColor = btnColor;
+    }
+
+    public int getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(int borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public void setOnClickStateChange(OnClickStateChange onClickStateChange) {
+        this.onClickStateChange = onClickStateChange;
+    }
+
+    public interface OnClickStateChange{
+        void onStateChange(int state,String statusText);
+    }
 }
